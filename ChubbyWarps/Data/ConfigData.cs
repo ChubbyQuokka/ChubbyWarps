ad94f94474293c6d7adc8d442c98e3217b1f473a
ChubbyQuokka <graybad1@icloud.com>
2018-08-23 14:25:20 -0400

    some stuff and license change

---
+++ ChubbyWarps/Data/ConfigData.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChubbyWarps.Data
+{
+    public sealed class ConfigData
+    {
+    }
+}
--- ChubbyWarps/Services/ConfigDataProvider.cs
+++ ChubbyWarps/Services/ConfigDataProvider.cs
@@ -1,8 +1,9 @@
-﻿using Rocket.API.Configuration;
+﻿using ChubbyWarps.Data;
+using Rocket.API.Configuration;
 
 namespace ChubbyWarps.Services
 {
-    public sealed class ConfigDataProvider : DataProvider<object>
+    public sealed class ConfigDataProvider : DataProvider<ConfigData>
     {
         public ConfigDataProvider(IConfiguration configuration) : base(configuration) { }
 
